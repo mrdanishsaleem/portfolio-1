81d4d6cfb4a063835cc57f696afe6564330bde2a
Danish Saleem <danish_s@outlook.com>
2022-03-19 23:44:41 +0500

    Update Projects.section.tsx

--- components/Sections/Projects.section.tsx
+++ components/Sections/Projects.section.tsx
@@ -14,6 +14,61 @@ const Projects: React.FC = () => {
             rel="noopener noreferrer"
           >
             <div className="bg-primary flex h-full w-full flex-col items-center justify-center rounded-lg px-2 text-center font-medium">
+              <p className="text-xl font-semibold">Junaid Umer</p>
+              <p>
+                An open-source portfolio built in ReactJS and proudly hosted on
+                Netlify.
+              </p>
+            </div>
+          </a>
+        </Link>
+
+        <Link href="https://github.com/avneesh0612/Orbits-UI" passHref>
+          <a
+            className="h-[7rem] w-[14rem] cursor-pointer rounded-lg bg-gradient-to-r from-[#FDE68A] via-[#FCA5A5] to-[#FECACA] p-1 text-white duration-100 hover:scale-105"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div className="bg-primary flex h-full w-full flex-col items-center justify-center rounded-lg px-2 text-center font-medium">
+              <p className="text-xl font-semibold">Orbits UI</p>
+              <p>React component library built with Tailwind CSS</p>
+            </div>
+          </a>
+        </Link>
+
+        <Link href="https://vault3.live" passHref>
+          <a
+            className="h-[7rem] w-[14rem] cursor-pointer rounded-lg bg-gradient-to-r from-[#D8B4FE] to-[#818CF8] p-1 text-white duration-100 hover:scale-105"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div className="bg-primary flex h-full w-full flex-col items-center justify-center rounded-lg px-2 text-center font-medium">
+              <p className="text-xl font-semibold">Vault3</p>
+              <p>Your safest decentralized vault</p>
+            </div>
+          </a>
+        </Link>
+
+        <Link href="https://github.com/avneesh0612/Orbits-UI" passHref>
+          <a
+            className="h-[7rem] w-[14rem] cursor-pointer rounded-lg bg-gradient-to-r from-[#FDE68A] via-[#FCA5A5] to-[#FECACA] p-1 text-white duration-100 hover:scale-105"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div className="bg-primary flex h-full w-full flex-col items-center justify-center rounded-lg px-2 text-center font-medium">
+              <p className="text-xl font-semibold">Orbits UI</p>
+              <p>React component library built with Tailwind CSS</p>
+            </div>
+          </a>
+        </Link>
+
+        <Link href="https://vault3.live" passHref>
+          <a
+            className="h-[7rem] w-[14rem] cursor-pointer rounded-lg bg-gradient-to-r from-[#D8B4FE] to-[#818CF8] p-1 text-white duration-100 hover:scale-105"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div className="bg-primary flex h-full w-full flex-col items-center justify-center rounded-lg px-2 text-center font-medium">
               <p className="text-xl font-semibold">Vault3</p>
               <p>Your safest decentralized vault</p>
             </div>
