d314fe9eeb57030bb151f82b3722605c553ce55d
Danish Saleem <danish_s@outlook.com>
2022-03-19 23:01:30 +0500

    Update Contact.section.tsx

--- components/Sections/Contact.section.tsx
+++ components/Sections/Contact.section.tsx
@@ -9,22 +9,25 @@ import { Icon } from "../Misc/Icon.component";
 
 const Contact = () => {
   return (
-    <div className="px-3 font-sen" id="contact">
+    <div className="font-sen px-3" id="contact">
       <p className="text-3xl font-bold text-white">Get in touch</p>
 
       <div className="my-8 flex flex-row justify-center gap-x-4">
-        <Icon icon={<AiOutlineGithub />} url="https://github.com/kr-anurag" />
+        <Icon
+          icon={<AiOutlineGithub />}
+          url="https://github.com/mrdanishsaleem"
+        />
 
         <Icon
           icon={<AiOutlineTwitter />}
-          url="https://twitter.com/kr_anurag_"
+          url="https://twitter.com/MrDanishSaleem"
         />
 
-        <Icon icon={<MdEmail />} url="mailto:kr.anurag24@gmail.com" />
+        <Icon icon={<MdEmail />} url="mailto:danish_s@outlook.com" />
 
         <Icon
           icon={<BsSpotify />}
-          url="https://open.spotify.com/user/31l2fev6wn2mtzclbpvzxis3x77q"
+          url="https://open.spotify.com/user/qgu3xe8fo0k432q4nu3tbfzj5"
         />
       </div>
     </div>
