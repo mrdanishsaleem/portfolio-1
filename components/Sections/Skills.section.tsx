a1fa0437fa979a1c2307f0b328b019ea7fc85108
Danish Saleem <danish_s@outlook.com>
2022-03-19 23:08:59 +0500

    Update Skills.section.tsx

--- components/Sections/Skills.section.tsx
+++ components/Sections/Skills.section.tsx
@@ -2,19 +2,19 @@ import { BsArrowRightShort } from "../Misc/Icons.collection";
 
 const Skills = () => {
   return (
-    <div className="my-16 px-3 font-sen text-white" id="skills">
+    <div className="font-sen my-16 px-3 text-white" id="skills">
       <p className="text-3xl font-bold text-white">Skills & Uses</p>
 
-      <div className="text-md my-8 flex flex-col font-medium md:text-xl custom:text-lg">
+      <div className="text-md custom:text-lg my-8 flex flex-col font-medium md:text-xl">
         <p className="flex flex-row items-center border-b-[0.1px] border-gray-500 py-1 text-slate-300">
           <BsArrowRightShort size="30" />
-          <span className="text-white">NextJS</span>
+          <span className="text-white">ReactJS & NextJS</span>
           &nbsp;as my Frontend Framework
         </p>
 
         <p className="flex flex-row items-center border-b-[0.1px] border-gray-500 py-1 text-slate-300">
           <BsArrowRightShort size="30" />
-          <span className="text-white">Typescript </span>
+          <span className="text-white">JavaScript & Typescript </span>
           &nbsp;as my main language
         </p>
 
@@ -26,7 +26,7 @@ const Skills = () => {
 
         <p className="flex flex-row items-center border-b-[0.1px] border-gray-500 py-1 text-slate-300">
           <BsArrowRightShort size="30" />
-          <span className="text-white">Figma </span>
+          <span className="text-white">Adobe XD </span>
           &nbsp;as my designing tool
         </p>
 
@@ -35,12 +35,26 @@ const Skills = () => {
           <span className="text-white">Express JS </span>
           &nbsp;as my API framework
         </p>
+
+        <p className="flex flex-row items-center border-b-[0.1px] border-gray-500 py-1 text-slate-300">
+          <BsArrowRightShort size="30" />
+          <span className="text-white">Git & GitHub </span>
+          &nbsp;as my VCSs
+        </p>
+
+        <p className="flex flex-row items-center border-b-[0.1px] border-gray-500 py-1 text-slate-300">
+          <BsArrowRightShort size="30" />
+          <span className="text-white">Visual Studio Code </span>
+          &nbsp;as my code editor
+        </p>
       </div>
 
       <p className="text-lg font-medium text-slate-300">
-        ...more skills include <span className="text-white">javascript</span>,{" "}
-        <span className="text-white">reactjs</span>,{" "}
-        <span className="text-white">fastapi </span>{" "}
+        ...more skills include <span className="text-white">HTML5</span>,{" "}
+        <span className="text-white">CSS3</span>,{" "}
+        <span className="text-white">Automation</span>,{" "}
+        <span className="text-white">Databases</span>,{" "}
+        <span className="text-white">RapidAPI</span>{" "}
       </p>
     </div>
   );
