f3453bb9a50e65bf044ffb84d965e3e75674e654
Danish Saleem <danish_s@outlook.com>
2022-04-25 21:55:52 +0500

    Update Projects.section.tsx
    Add new project card

--- components/Sections/Projects.section.tsx
+++ components/Sections/Projects.section.tsx
@@ -46,6 +46,19 @@ const Projects: React.FC = () => {
           </a>
         </Link>
 
+        <Link href="https://web-with-video.vercel.app/" passHref>
+          <a
+            className="h-[7rem] w-[14rem] cursor-pointer rounded-lg bg-gradient-to-r from-[#D8B4FE] to-[#818CF8] p-1 text-white duration-100 hover:scale-105"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div className="flex h-full w-full flex-col items-center justify-center rounded-lg bg-primary px-2 text-center font-medium">
+              <p className="text-xl font-semibold">Blessed Couples</p>
+              <p>Never stop exploring the World!</p>
+            </div>
+          </a>
+        </Link>
+
         {/* <Link href="https://gluconatural.com/" passHref>
           <a
             className="h-[7rem] w-[14rem] cursor-pointer rounded-lg bg-gradient-to-r from-[#FDE68A] via-[#FCA5A5] to-[#FECACA] p-1 text-white duration-100 hover:scale-105"
