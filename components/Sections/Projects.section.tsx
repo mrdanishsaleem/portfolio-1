fc125cca49bd107a76a62502a25f5e9881350923
Danish Saleem <danish_s@outlook.com>
2022-05-02 11:55:12 +0500

    Revert "Update files"
    This reverts commit 40ae90ed3a98e68a3c03ce550bb69f68787956cf.

--- components/Sections/Projects.section.tsx
+++ components/Sections/Projects.section.tsx
@@ -7,18 +7,18 @@ const Projects: React.FC = () => {
     <div className="my-16 px-3 font-sen" id="projects">
       <p className="text-3xl font-bold text-white">Featured Projects</p>
       <div className="my-8 flex flex-col items-center justify-center gap-10 sm:flex-row">
-        {/* <Link href="https://junaidumer.netlify.app/" passHref>
+        <Link href="https://junaidumer.netlify.app/" passHref>
           <a
             className="h-[7rem] w-[14rem] cursor-pointer rounded-lg bg-gradient-to-r from-[#D8B4FE] to-[#818CF8] p-1 text-white duration-100 hover:scale-105"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <div className="bg-primary flex h-full w-full flex-col items-center justify-center rounded-lg px-2 text-center font-medium">
+            <div className="flex h-full w-full flex-col items-center justify-center rounded-lg bg-primary px-2 text-center font-medium">
               <p className="text-xl font-semibold">Junaid Umer</p>
               <p>Open-source portfolio website</p>
             </div>
           </a>
-        </Link> */}
+        </Link>
 
         <Link href="https://www.beyonddispatch.com/" passHref>
           <a
@@ -46,20 +46,20 @@ const Projects: React.FC = () => {
           </a>
         </Link>
 
-        {/* <Link href="https://web-with-video.vercel.app/" passHref>
-          <a
+        <Link href="https://web-with-video.vercel.app/" passHref>
+        <a
             className="h-[7rem] w-[14rem] cursor-pointer rounded-lg bg-gradient-to-r from-[#FDE68A] via-[#FCA5A5] to-[#FECACA] p-1 text-white duration-100 hover:scale-105"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <div className="bg-primary flex h-full w-full flex-col items-center justify-center rounded-lg px-2 text-center font-medium">
+            <div className="flex h-full w-full flex-col items-center justify-center rounded-lg bg-primary px-2 text-center font-medium">
               <p className="text-xl font-semibold">Website with Video</p>
               <p>Never stop exploring the World!</p>
             </div>
           </a>
-        </Link> */}
+        </Link>
 
-        <Link href="https://gluconatural.com/" passHref>
+        {/* <Link href="https://gluconatural.com/" passHref>
           <a
             className="h-[7rem] w-[14rem] cursor-pointer rounded-lg bg-gradient-to-r from-[#FDE68A] via-[#FCA5A5] to-[#FECACA] p-1 text-white duration-100 hover:scale-105"
             target="_blank"
@@ -85,7 +85,7 @@ const Projects: React.FC = () => {
               </p>
             </div>
           </a>
-        </Link>
+        </Link> */}
       </div>
     </div>
   );
