0f8d5dfd28e96578b851db7da4d6602fc383d6c4
Danish Saleem <danish_s@outlook.com>
2022-05-02 18:28:43 +0500

    Update Projects.section.tsx

--- components/Sections/Projects.section.tsx
+++ components/Sections/Projects.section.tsx
@@ -4,10 +4,10 @@ import Link from "next/link";
 
 const Projects: React.FC = () => {
   return (
-    <div className="my-16 px-3 font-sen" id="projects">
+    <div className="font-sen my-16 px-3" id="projects">
       <p className="text-3xl font-bold text-white">Featured Projects</p>
       <div className="my-8 flex flex-col items-center justify-center gap-10 sm:flex-row">
-        <Link href="https://junaidumer.netlify.app/" passHref>
+        {/* <Link href="https://junaidumer.netlify.app/" passHref>
           <a
             className="h-[7rem] w-[14rem] cursor-pointer rounded-lg bg-gradient-to-r from-[#D8B4FE] to-[#818CF8] p-1 text-white duration-100 hover:scale-105"
             target="_blank"
@@ -18,7 +18,7 @@ const Projects: React.FC = () => {
               <p>Open-source portfolio website</p>
             </div>
           </a>
-        </Link>
+        </Link> */}
 
         <Link href="https://www.beyonddispatch.com/" passHref>
           <a
@@ -26,7 +26,7 @@ const Projects: React.FC = () => {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <div className="flex h-full w-full flex-col items-center justify-center rounded-lg bg-primary px-2 text-center font-medium">
+            <div className="bg-primary flex h-full w-full flex-col items-center justify-center rounded-lg px-2 text-center font-medium">
               <p className="text-xl font-semibold">Beyond Dispatch</p>
               <p>#1 truck dispatcher for owner-operators!</p>
             </div>
@@ -39,7 +39,7 @@ const Projects: React.FC = () => {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <div className="flex h-full w-full flex-col items-center justify-center rounded-lg bg-primary px-2 text-center font-medium">
+            <div className="bg-primary flex h-full w-full flex-col items-center justify-center rounded-lg px-2 text-center font-medium">
               <p className="text-xl font-semibold">Blessed Couples</p>
               <p>Match making service with a new idea!</p>
             </div>
@@ -52,25 +52,25 @@ const Projects: React.FC = () => {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <div className="flex h-full w-full flex-col items-center justify-center rounded-lg bg-primary px-2 text-center font-medium">
+            <div className="bg-primary flex h-full w-full flex-col items-center justify-center rounded-lg px-2 text-center font-medium">
               <p className="text-xl font-semibold">Website with Video</p>
               <p>Never stop exploring the World!</p>
             </div>
           </a>
         </Link>
 
-        <Link href="https://gluconatural.com/" passHref>
+        {/* <Link href="https://gluconatural.com/" passHref>
           <a
             className="h-[7rem] w-[14rem] cursor-pointer rounded-lg bg-gradient-to-r from-[#FDE68A] via-[#FCA5A5] to-[#FECACA] p-1 text-white duration-100 hover:scale-105"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <div className="flex h-full w-full flex-col items-center justify-center rounded-lg bg-primary px-2 text-center font-medium">
+            <div className="bg-primary flex h-full w-full flex-col items-center justify-center rounded-lg px-2 text-center font-medium">
               <p className="text-xl font-semibold">Gluco Natural</p>
               <p>Natural alternate to manage your diabetes.</p>
             </div>
           </a>
-        </Link>
+        </Link> */}
 
         <Link href="https://www.wandrpass.com/" passHref>
           <a
@@ -78,7 +78,7 @@ const Projects: React.FC = () => {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <div className="flex h-full w-full flex-col items-center justify-center rounded-lg bg-primary px-2 text-center font-medium">
+            <div className="bg-primary flex h-full w-full flex-col items-center justify-center rounded-lg px-2 text-center font-medium">
               <p className="text-xl font-semibold">WandrPass</p>
               <p>
                 Find the best local restaurants and access exclusive promotions!
