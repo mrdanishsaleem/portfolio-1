ef18d8212bf29df84e7f213733f1f3f0a23170cb
Danish Saleem <danish_s@outlook.com>
2022-05-02 11:25:39 +0500

    Update Projects.section.tsx

--- components/Sections/Projects.section.tsx
+++ components/Sections/Projects.section.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 const Projects: React.FC = () => {
   return (
-    <div className="font-sen my-16 px-3" id="projects">
+    <div className="my-16 px-3 font-sen" id="projects">
       <p className="text-3xl font-bold text-white">Featured Projects</p>
       <div className="my-8 flex flex-col items-center justify-center gap-10 sm:flex-row">
         {/* <Link href="https://junaidumer.netlify.app/" passHref>
@@ -26,7 +26,7 @@ const Projects: React.FC = () => {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <div className="bg-primary flex h-full w-full flex-col items-center justify-center rounded-lg px-2 text-center font-medium">
+            <div className="flex h-full w-full flex-col items-center justify-center rounded-lg bg-primary px-2 text-center font-medium">
               <p className="text-xl font-semibold">Beyond Dispatch</p>
               <p>#1 truck dispatcher for owner-operators!</p>
             </div>
@@ -39,14 +39,13 @@ const Projects: React.FC = () => {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <div className="bg-primary flex h-full w-full flex-col items-center justify-center rounded-lg px-2 text-center font-medium">
+            <div className="flex h-full w-full flex-col items-center justify-center rounded-lg bg-primary px-2 text-center font-medium">
               <p className="text-xl font-semibold">Blessed Couples</p>
               <p>Match making service with a new idea!</p>
             </div>
           </a>
         </Link>
 
-<<<<<<< HEAD
         {/* <Link href="https://web-with-video.vercel.app/" passHref>
           <a
             className="h-[7rem] w-[14rem] cursor-pointer rounded-lg bg-gradient-to-r from-[#FDE68A] via-[#FCA5A5] to-[#FECACA] p-1 text-white duration-100 hover:scale-105"
@@ -55,16 +54,6 @@ const Projects: React.FC = () => {
           >
             <div className="bg-primary flex h-full w-full flex-col items-center justify-center rounded-lg px-2 text-center font-medium">
               <p className="text-xl font-semibold">Website with Video</p>
-=======
-        <Link href="https://web-with-video.vercel.app/" passHref>
-          <a
-            className="h-[7rem] w-[14rem] cursor-pointer rounded-lg bg-gradient-to-r from-[#D8B4FE] to-[#818CF8] p-1 text-white duration-100 hover:scale-105"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <div className="flex h-full w-full flex-col items-center justify-center rounded-lg bg-primary px-2 text-center font-medium">
-              <p className="text-xl font-semibold">Blessed Couples</p>
->>>>>>> parent of e013d47 (Update Projects.section.tsx)
               <p>Never stop exploring the World!</p>
             </div>
           </a>
@@ -76,7 +65,7 @@ const Projects: React.FC = () => {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <div className="bg-primary flex h-full w-full flex-col items-center justify-center rounded-lg px-2 text-center font-medium">
+            <div className="flex h-full w-full flex-col items-center justify-center rounded-lg bg-primary px-2 text-center font-medium">
               <p className="text-xl font-semibold">Gluco Natural</p>
               <p>Natural alternate to manage your diabetes.</p>
             </div>
@@ -89,7 +78,7 @@ const Projects: React.FC = () => {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <div className="bg-primary flex h-full w-full flex-col items-center justify-center rounded-lg px-2 text-center font-medium">
+            <div className="flex h-full w-full flex-col items-center justify-center rounded-lg bg-primary px-2 text-center font-medium">
               <p className="text-xl font-semibold">WandrPass</p>
               <p>
                 Find the best local restaurants and access exclusive promotions!
