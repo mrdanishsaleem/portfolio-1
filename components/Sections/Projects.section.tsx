ae0f16cf032ba3d290f83ae5127b4c1239bcc11a
Danish Saleem <danish_s@outlook.com>
2022-05-21 00:24:45 +0500

    Updated projects and Avatar

--- components/Sections/Projects.section.tsx
+++ components/Sections/Projects.section.tsx
@@ -7,20 +7,21 @@ const Projects: React.FC = () => {
     <div className="font-sen my-16 px-3" id="projects">
       <p className="text-3xl font-bold text-white">Featured Projects</p>
       <div className="my-8 flex flex-col items-center justify-center gap-10 sm:flex-row">
-        {/* <Link href="https://junaidumer.netlify.app/" passHref>
+        <Link href="https://junaidumer.netlify.app/" passHref>
           <a
-            className="h-[7rem] w-[14rem] cursor-pointer rounded-lg bg-gradient-to-r from-[#D8B4FE] to-[#818CF8] p-1 text-white duration-100 hover:scale-105"
+            className="h-[7rem] w-[14rem] cursor-pointer rounded-lg bg-gradient-to-r from-[#FDE68A] via-[#FCA5A5] to-[#FECACA] p-1 text-white duration-100 hover:scale-105"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <div className="flex h-full w-full flex-col items-center justify-center rounded-lg bg-primary px-2 text-center font-medium">
+            <div className="bg-primary flex h-full w-full flex-col items-center justify-center rounded-lg px-2 text-center font-medium">
+              {" "}
               <p className="text-xl font-semibold">Junaid Umer</p>
               <p>Open-source portfolio website</p>
             </div>
           </a>
-        </Link> */}
+        </Link>
 
-        <Link href="https://www.beyonddispatch.com/" passHref>
+        {/* <Link href="https://www.beyonddispatch.com/" passHref>
           <a
             className="h-[7rem] w-[14rem] cursor-pointer rounded-lg bg-gradient-to-r from-[#FDE68A] via-[#FCA5A5] to-[#FECACA] p-1 text-white duration-100 hover:scale-105"
             target="_blank"
@@ -31,7 +32,7 @@ const Projects: React.FC = () => {
               <p>#1 truck dispatcher!</p>
             </div>
           </a>
-        </Link>
+        </Link> */}
 
         <Link href="https://blessedcouples.pk/" passHref>
           <a
