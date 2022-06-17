7a8320e7f1a61b5c6278f05661bedd1a8b5ab457
Danish Saleem <danish_s@outlook.com>
2022-06-18 02:33:52 +0500

    Remove Dazvix from Projects.section.jsx file and format all files with prettier --write . command

--- components/Sections/Projects.section.tsx
+++ components/Sections/Projects.section.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 const Projects: React.FC = () => {
   return (
-    <div className="font-sen my-16 px-3" id="projects">
+    <div className="my-16 px-3 font-sen" id="projects">
       <p className="text-3xl font-bold text-white">Featured Projects</p>
       <div className="my-8 flex flex-col items-center justify-center gap-10 sm:flex-row">
         <Link href="https://junaidumer.netlify.app/" passHref>
@@ -13,25 +13,12 @@ const Projects: React.FC = () => {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <div className="bg-primary flex h-full w-full flex-col items-center justify-center rounded-lg px-2 text-center font-medium">
+            <div className="flex h-full w-full flex-col items-center justify-center rounded-lg bg-primary px-2 text-center font-medium">
               <p className="text-xl font-semibold">Junaid Umer</p>
               <p>A Portfolio website</p>
             </div>
           </a>
         </Link>
-
-        {/* <Link href="https://www.beyonddispatch.com/" passHref>
-          <a
-            className="h-[7rem] w-[14rem] cursor-pointer rounded-lg bg-gradient-to-r from-[#FDE68A] via-[#FCA5A5] to-[#FECACA] p-1 text-white duration-100 hover:scale-105"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <div className="bg-primary flex h-full w-full flex-col items-center justify-center rounded-lg px-2 text-center font-medium">
-              <p className="text-xl font-semibold">Beyond Dispatch</p>
-              <p>#1 truck dispatcher!</p>
-            </div>
-          </a>
-        </Link> */}
 
         <Link href="https://blessedcouples.pk/" passHref>
           <a
@@ -39,33 +26,20 @@ const Projects: React.FC = () => {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <div className="bg-primary flex h-full w-full flex-col items-center justify-center rounded-lg px-2 text-center font-medium">
+            <div className="flex h-full w-full flex-col items-center justify-center rounded-lg bg-primary px-2 text-center font-medium">
               <p className="text-xl font-semibold">Blessed Couples</p>
               <p>Match making!</p>
             </div>
           </a>
         </Link>
 
-        <Link href="https://dazvix.vercel.app/" passHref>
+        <Link href="https://www.wandrpass.com/" passHref>
           <a
             className="h-[7rem] w-[14rem] cursor-pointer rounded-lg bg-gradient-to-r from-[#FDE68A] via-[#FCA5A5] to-[#FECACA] p-1 text-white duration-100 hover:scale-105"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <div className="bg-primary flex h-full w-full flex-col items-center justify-center rounded-lg px-2 text-center font-medium">
-              <p className="text-xl font-semibold">Dazvix</p>
-              <p>Agency with client satisfaction</p>
-            </div>
-          </a>
-        </Link>
-
-        <Link href="https://www.wandrpass.com/" passHref>
-          <a
-            className="h-[7rem] w-[14rem] cursor-pointer rounded-lg bg-gradient-to-r from-[#D8B4FE] to-[#818CF8] p-1 text-white duration-100 hover:scale-105"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <div className="bg-primary flex h-full w-full flex-col items-center justify-center rounded-lg px-2 text-center font-medium">
+            <div className="flex h-full w-full flex-col items-center justify-center rounded-lg bg-primary px-2 text-center font-medium">
               <p className="text-xl font-semibold">WandrPass</p>
               <p>Access to exclusive promotions!</p>
             </div>
@@ -78,7 +52,7 @@ const Projects: React.FC = () => {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <div className="bg-primary flex h-full w-full flex-col items-center justify-center rounded-lg px-2 text-center font-medium">
+            <div className="flex h-full w-full flex-col items-center justify-center rounded-lg bg-primary px-2 text-center font-medium">
               <p className="text-xl font-semibold">Travel World</p>
               <p>Explore de world!</p>
             </div>
